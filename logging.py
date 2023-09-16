2ec933c9bc24ee0930723ed75e8d15c31b093809
Wizz_^ <lewizard@hotmail.fr>
2023-09-16 11:39:59 +0200

    Delete logging.py

--- logging.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-# Configure basic logger
-logging.basicConfig(level=logging.INFO,
-                    format='[%(asctime)s] %(levelname)s: %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
-
-def get_logger(name):
-    return logging.getLogger(name)
