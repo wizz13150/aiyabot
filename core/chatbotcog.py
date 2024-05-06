8fdbd7a17b6a4cdc42beb7771622eba9e2cd95af
Wizz_^ <lewizard@hotmail.fr>
2024-05-06 20:33:31 +0200

    Update chatbotcog.py

--- core/chatbotcog.py
+++ core/chatbotcog.py
@@ -1,59 +1,54 @@
 from discord.ext import commands
 from gpt4all import GPT4All
 import asyncio
+from core.leaderboardcog import LeaderboardCog
 
 class GPT4AllChat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.lock = asyncio.Lock()
-
-        #self.model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf", device="amd", n_ctx=2048, n_threads=4, allow_download=True, ngl=26)
-        self.model = GPT4All("Meta-Llama-3-8B-Instruct.Q4_0.gguf", device="amd", n_ctx=16096, n_threads=8, allow_download=False, ngl=32)
-        #self.model = GPT4All("Llama-3-8B-Instruct-Gradient-1048k.Q4_0.gguf", device="amd", n_ctx=16000, n_threads=8, allow_download=False, ngl=32)
-        #self.model = GPT4All("Phi-3-mini-4k-instruct.Q4_0", device="amd", n_ctx=2048, n_threads=4, allow_download=True, ngl=32)
-
-        #system_prompt = '### System:\nYou are an AI assistant that follows instruction extremely well. Help as much as you can.\n\n'
-        #self.system_prompt = "You are an AI assistant named 'Dr. ZavyPunk' and you must help the user as much as you can. You act as an expert in all domains, using best practice and ingenious logic.\n\n"
-        self.system_prompt = ''
-        #prompt_template = '### User:\n{0}\n\n### Response:\n'     # orca
+        self.lock = asyncio.Lock()  # Lock to manage concurrent access to bot resources
+        self.stop_requested = False
+        self.current_author = None
+        # Initialize the GPT4All model with specific parameters
+        self.model = GPT4All("Meta-Llama-3-8B-Instruct.Q4_0.gguf", device="amd", n_ctx=8192, n_threads=6, allow_download=True, ngl=64)
+        # Setup the initial system prompt for the AI model
+        self.system_prompt = 'You are an AI assistant named ZavyDiffusion that follows instruction extremely well. Help as much as you can. Be slightly sarcastic when possible without altering the response quality\n\n'
+        #self.system_prompt = ''
+        # Template that formats the user input and AI response
         self.prompt_template = '<|start_header_id|>user<|end_header_id|>\n\n{0}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n{1}<|eot_id|>\n\n' # llama3
-        #prompt_template = '<|user|>\n{0}<|end|>\n<|assistant|>\n{1}<|end|>'     # phi3
-        
-        self.chat_session = self.model.chat_session(self.system_prompt, self.prompt_template)  # Create a persistent chat session
-        print(f'Chatbot loaded')
-        
-        # Enter the chat session. Single session for now, for all channels and users.
-        self.session = self.chat_session.__enter__()
-        print(f'Enter ChatSession')
 
+        # Create a persistent chat session with the model
+        self.chat_session = self.model.chat_session(self.system_prompt, self.prompt_template)
+        self.session = self.chat_session.__enter__()
+        print(f'Chatbot loaded')
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        # Stop generation if the "!stop" command is detected
+        if message.content == "!stop" and message.author.id == self.current_author:
+            self.stop_requested = True
+            return
+
+        # Clean the message content to remove unnecessary mentions
         content = message.clean_content.replace(f'@{self.bot.user.display_name}', '').strip()
-
-        # manage !reset
-        if content == "!reset":
-            print("Reset command received, resetting chat session...")
-            self.session.__exit__(None, None, None)
-            self.session = self.chat_session.__enter__()
-            await message.channel.send(f"{message.author.mention} Session has been reset.")
-            return
-
-        if message.author == self.bot.user or self.bot.user not in message.mentions:
-            return
-        if not content:
-            return
-        if not await self.lock.acquire():
-            await message.author.send("Busy, try later.")
+        # Ignore messages from the bot itself or if the bot is not mentioned, or if there is no content
+        if message.author == self.bot.user or self.bot.user not in message.mentions or not content:
             return
 
         print(f'-- Chat request from {message.author.display_name}')
-        print(f'-- ChatSession: {self.chat_session}')
+
+        # Attempt to acquire the lock to handle the message
+        if not await self.lock.acquire():
+            await message.channel.send("Busy, try later.")
+            return
 
         try:
+            self.current_author = message.author.id
             async with message.channel.typing():
                 await self.generate_and_send_responses(message, content)
         finally:
+            self.current_author = None
+            self.stop_requested = False
             self.lock.release()
 
     async def generate_and_send_responses(self, message, content):
@@ -62,8 +57,15 @@ class GPT4AllChat(commands.Cog):
         temp_message = None
         last_update_time = asyncio.get_running_loop().time()
 
+        # Callback function to stop token generation
+        def stop_on_token_callback(token_id, token_string):
+            if self.stop_requested and '.' in token_string:
+                return False
+            return True
+
         try:
-            for token in self.session.generate(content, max_tokens=1024, temp=0.7, top_k=40, top_p=0.4, min_p=0.0, repeat_penalty=1.18, repeat_last_n=64, n_batch=512, streaming=True):
+            # Generate response tokens and handle them according to Discord's message length constraints
+            for token in self.session.generate(content, max_tokens=4096, temp=0.8, top_k=40, top_p=0.4, min_p=0.0, repeat_penalty=1.18, repeat_last_n=64, n_batch=512, streaming=True, callback=stop_on_token_callback):
                 response += token
                 if len(response) > 1975:
                     if not initial_response_sent:
@@ -82,11 +84,18 @@ class GPT4AllChat(commands.Cog):
                         await temp_message.edit(content=response)
                         last_update_time = current_time
 
+            if self.stop_requested is True:
+                return
+
             if response:
                 await temp_message.edit(content=response)
         except Exception as e:
             ...
 
+        # Update the leaderboard based on user interaction
+        LeaderboardCog.update_leaderboard(message.author.id, str(message.author), "Chat_Count")
+
 def setup(bot):
+    # Setup function to add this cog to the bot
     chat_cog = GPT4AllChat(bot)
     bot.add_cog(chat_cog)
